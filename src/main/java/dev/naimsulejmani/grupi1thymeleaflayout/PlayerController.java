90afb62af28fcec670128bb160fc61f6068ae011
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-11-14 12:25:02 +0100

    Edit-player

--- src/main/java/dev/naimsulejmani/grupi1thymeleaflayout/PlayerController.java
+++ src/main/java/dev/naimsulejmani/grupi1thymeleaflayout/PlayerController.java
@@ -3,6 +3,7 @@ package dev.naimsulejmani.grupi1thymeleaflayout;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,6 +32,8 @@ public class PlayerController {
             , @RequestParam(value = "search", required = false) String search
 //            , @RequestParam(value="test", required = false) int test
     ) {
+
+        mav.addObject("search", search);
         mav.setViewName("players"); // templates/players.html
         mav.addObject("pageTitle", "Players Page");
 //        mav.addObject("players", players);
@@ -51,6 +54,34 @@ public class PlayerController {
 //    public String players(Model model) {
 //        model.addAttribute("pageTitle", "Players Page");
 //        return "players";
+//    }
+
+    //get players by id
+    // http://localhost:8080/players/1
+    @GetMapping("/{id}")
+    public String player(Model model, @PathVariable("id") int id) {
+        var player = players.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+        model.addAttribute("player", player);
+        return "player";
+    }
+
+    //http://localhost:8080/players/1/edit
+    @GetMapping("/{id}/edit")
+    public String editPlayer(Model model, @PathVariable("id") int id) {
+        var player = players.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+        model.addAttribute("player", player);
+        return "edit-player";
+    }
+
+//    public Player findFirstPlayerById(int id) {
+//        Player player = null;
+//        for (Player p : players) {
+//            if (p.getId() == id) {
+//                player = p;
+//                break;
+//            }
+//        }
+//        return player;
 //    }
 
 
